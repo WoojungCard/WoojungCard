0683853fb8a4b8e4de509a60574a8f71cd18dd9a
eblyncho <eblyn.cho@gmail.com>
2023-07-18 16:25:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/woojungcard/woojungcard/WoojungcardApplication.java
@@ -0,0 +1,13 @@
+package com.woojungcard.woojungcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoojungcardApplication {
+
+		public static void main(String[] args) {
+		SpringApplication.run(WoojungcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woojungcard/woojungcard/WoojungcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojungcard.woojungcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoojungcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
