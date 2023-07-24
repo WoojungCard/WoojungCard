ce7ec07fbb47a24aecfab852c968c4a00723a04d
pshoon25 <pshoon25@naver.com>
2023-07-24 16:55:40 +0900

    Lombok Setting

--- src/main/java/com/woojungcard/woojungcard/controller/UserController.java
+++ src/main/java/com/woojungcard/woojungcard/controller/UserController.java
@@ -2,32 +2,19 @@ package com.woojungcard.woojungcard.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.woojungcard.woojungcard.dto.UserDTO;
 import com.woojungcard.woojungcard.service.UserService;
 
 import lombok.RequiredArgsConstructor;
-
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 	
 	private final UserService userService;
-	
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
 	
 	@GetMapping("/test")
 	public List<UserDTO> test() {
--- src/main/java/com/woojungcard/woojungcard/dto/UserDTO.java
+++ src/main/java/com/woojungcard/woojungcard/dto/UserDTO.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+
+@AllArgsConstructor 
 @NoArgsConstructor
 @Getter @Setter
 public class UserDTO {
@@ -19,52 +20,4 @@ public class UserDTO {
 	private String userGender;
 	private String userTel;
 	private LocalDate userJoinDate;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUserPwd() {
-		return userPwd;
-	}
-	public void setUserPwd(String userPwd) {
-		this.userPwd = userPwd;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public LocalDate getUserBirth() {
-		return userBirth;
-	}
-	public void setUserBirth(LocalDate userBirth) {
-		this.userBirth = userBirth;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserGender() {
-		return userGender;
-	}
-	public void setUserGender(String userGender) {
-		this.userGender = userGender;
-	}
-	public String getUserTel() {
-		return userTel;
-	}
-	public void setUserTel(String userTel) {
-		this.userTel = userTel;
-	}
-	public LocalDate getUserJoinDate() {
-		return userJoinDate;
-	}
-	public void setUserJoinDate(LocalDate userJoinDate) {
-		this.userJoinDate = userJoinDate;
-	}
 }
